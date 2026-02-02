3669eac9904eb61154f55833770c9c41f1b64d52
James Bush <jamesalanbush1972@gmail.com>
2026-02-01 23:57:28 -0500

    Initial Commit

---
+++ APIBibleTest-2/APIBibleTest_2App.swift
@@ -0,0 +1,17 @@
+//
+//  APIBibleTest_2App.swift
+//  APIBibleTest-2
+//
+//  Created by Xcode Developer on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct APIBibleTest_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
